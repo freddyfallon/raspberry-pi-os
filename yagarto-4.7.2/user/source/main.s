3207c1c50c34e543e90682d2fcb68f55035e3a66
Jessica Rodriguez <jrrodriguez84@gmail.com>
2017-05-11 10:23:57 +0100

    WIP screen lesson create getmailboxbase and write

--- yagarto-4.7.2/user/source/main.s
+++ yagarto-4.7.2/user/source/main.s
@@ -15,4 +15,33 @@ mov pc,lr
 
 .globl MailboxWrite
 MailboxWrite:
-tst
+tst r0,#0b1111
+movne pc,lr
+cmp r1,#15
+movhi pc,lr
+
+channel .req r1
+value .req r2
+mov value,r0
+push {lr}
+bl GetMailboxBase
+mailbox .req r0
+
+wait1$:
+status .req r3
+ldr status,[mailbox,#0x18]
+
+tst status,#0x80000000
+.unreq status
+bne wait1$
+
+add value,channel
+.unreq channel
+
+str value,[mailbox,#0x20]
+.unreq value
+.unreq mailbox
+pop {pc}
+
+
+
