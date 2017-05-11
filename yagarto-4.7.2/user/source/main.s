4f5abaaca992e507a76cf800be53a50bbcf99dff
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-05-11 11:20:59 +0100

    adds frame buffer information

--- yagarto-4.7.2/user/source/main.s
+++ yagarto-4.7.2/user/source/main.s
@@ -43,5 +43,33 @@ str value,[mailbox,#0x20]
 .unreq mailbox
 pop {pc}
 
+.globl MailboxRead
+MailboxRead:
+cmp r0,#15
+movhi pc,lr
 
+channel .req r1
+mov channel,r0
+push {lr}
+bl GetMailboxBase
+mailbox .req r0
 
+rightmail$:
+wait2$:
+status .req r2
+ldr status,[mailbox,#0x18]
+
+mail .req r2
+ldr mail,[mailbox,#0]
+
+inchan .req r3
+and inchan,mail,#0b1111
+teq inchan,channel
+.unreq inchan
+bne rightmail$
+.unreq mailbox
+.unreq channel
+
+and r0,mail,#0xfffffff0
+.unreq mail
+pop {pc}
